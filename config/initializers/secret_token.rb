d2c41f4cbf6bdff3a7c52f82794f1586fc0a941f
OCC <occ@occ-R560.(none)>
2013-08-28 10:48:02 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LesEtoiles::Application.config.secret_key_base = '20b3d92c470d6b5613d5bc7801d88f9f565d52193959d9f4c796a08bcc87c820ff8289a7610f852a6770be7e50fa0abd2ee77e6061a4fc5c75ff9c1a2d4161ab'
