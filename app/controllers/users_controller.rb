7cbb0797cbf2c712e41894fb72705c4263f8355e
Oğuzhan İLHAN <ilhanoguzhan@gmail.com>
2013-08-29 16:53:45 +0300

    fix.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
   def index
     @test = User.find(current_user.id)
-
-
-
     if @test.profile.nil?
       @test = Profile.new
       @test.user_id = current_user.id
