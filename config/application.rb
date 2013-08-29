0675dd84f6d26106e35ace4875d21e2914a6dada
Orhun UZUNŞİMŞEK <orhun.uzunsimsek@gmail.com>
2013-08-29 18:06:18 +0300

    added pictures login screens

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -20,5 +21,6 @@ module LesEtoiles
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.i18n.default_locale = :tr
   end
 end
