eff7653de31040ce503b5d98fa5efe303488911a
Oğuzhan İLHAN <ilhanoguzhan@gmail.com>
2013-08-30 13:43:30 +0300

    #18 User profile clean and fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :profile, dependent: :destroy
+  accepts_nested_attributes_for :profile, :allow_destroy => true
 end
